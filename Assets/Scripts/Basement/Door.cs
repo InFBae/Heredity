91c21d5fa4121acb4bf255e4aaafc64086526300
YS AN <as5948@naver.com>
2023-09-04 15:39:18 +0900

    feat : develop open and pull interaction

---
+++ Assets/Scripts/Basement/Door.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Door : MonoBehaviour, IOpenable
+{
+	private HingeJoint hingeJoint;
+
+	[SerializeField]
+	private float openMinAngle;
+
+	[SerializeField]
+	private float openMaxAngle;
+
+	[SerializeField]
+	private float lockMinAngle;
+
+	[SerializeField]
+	private float lockMaxAngle;
+
+	private void Awake()
+	{
+		hingeJoint = gameObject.GetComponent<HingeJoint>();
+	
+
+		Lock();
+	}
+
+	public void Open()
+	{
+		
+	}
+
+	public void Unlock()
+	{
+		SetHingeAngle(openMinAngle, openMaxAngle);
+	}
+
+	public void Lock()
+	{
+		SetHingeAngle(lockMinAngle, lockMaxAngle);
+	}
+
+	private void SetHingeAngle(float minAngle, float maxAngle)
+	{
+		hingeJoint.useLimits = true;
+
+		JointLimits limits = hingeJoint.limits;
+		limits.min = minAngle;
+		limits.max = maxAngle;
+		hingeJoint.limits = limits;
+	}
+
+}
---
+++ Assets/Scripts/Basement/Drawer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Drawer : MonoBehaviour, IPullable
+{
+	private ConfigurableJoint configurableJoint;
+
+	[SerializeField]
+	private float limitLength;
+
+	[SerializeField]
+	private float lockLength;
+
+	private void Awake()
+	{
+		configurableJoint = gameObject.GetComponent<ConfigurableJoint>();
+	}
+
+	public void Pull()
+	{
+		
+	}
+
+	public void Unlock()
+	{
+		SetLinearLimit(limitLength);
+	}
+
+	public void Lock()
+	{
+		SetLinearLimit(lockLength);
+	}
+
+	private void SetLinearLimit(float length)
+	{
+		SoftJointLimit linearLimit = configurableJoint.linearLimit;
+		linearLimit.limit = length;
+		configurableJoint.linearLimit = linearLimit;
+	}
+}
