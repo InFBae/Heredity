7e55eab446a30b419ef299675636f39c6292cf91
JUNG BOBAE <acarid27@gmail.com>
2023-09-12 19:46:49 +0900

    fix: Fix Door Collider

--- Assets/Scenes/Gangbie/Scripts/BasicDoor.cs
+++ Assets/Scenes/Gangbie/Scripts/BasicDoor.cs
@@ -10,6 +10,15 @@ public class BasicDoor : Door
         Lock();
     }
 
+    private void Update()
+    {
+        if (hinge.angle <= hinge.limits.min + 1)
+        {
+            Debug.Log("Door Locked");
+            Lock();
+        }
+    }
+
     public override void Open()
     {
     }
@@ -19,7 +28,7 @@ public class BasicDoor : Door
         //Debug.Log($"Step : {step}");
         if (step == 1)
         {
-            Debug.Log("Basic Door Unlocked");
+            //Debug.Log("Basic Door Unlocked");
             Unlock();
         }
     }
