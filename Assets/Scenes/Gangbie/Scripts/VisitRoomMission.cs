07a4d4f47c0cba520757144a0e62d73bcffb6020
경빈 박 <gangbie1123@gmail.com>
2023-09-14 18:16:29 +0900

    feat : VisitRoom 기능 수정

--- Assets/Scenes/Gangbie/Scripts/VisitRoomMission.cs
+++ Assets/Scenes/Gangbie/Scripts/VisitRoomMission.cs
@@ -8,8 +8,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class VisitRoomMission : MonoBehaviour
 {
     [SerializeField] TMP_Text monitorText;
-    [SerializeField] Drawer drawer1;
-    [SerializeField] Drawer drawer3;
+    [SerializeField] AxisDragInteractable drawer1;
+    [SerializeField] AxisDragInteractable drawer3;
 
     private int socketSelectedCount = 0;
 
@@ -21,15 +21,18 @@ public class VisitRoomMission : MonoBehaviour
 
     private void Awake()
     {
-        drawer1.Lock();
-        drawer3.Lock();
+        // drawer1.AxisLength = 0;
+        // drawer3.AxisLength = 0;
+        // OnMissionComplete();
     }
 
     public void OnMissionComplete()
     {
         monitorText.text = "뒤의 철제 사물함 서랍에서 열쇠를 챙기세요";
-        drawer1.Unlock();
-        drawer3.Unlock();
+        drawer1.enabled = true;
+        drawer3.enabled = true;
+        drawer1.AxisLength = 0.35f;
+        drawer3.AxisLength = 0.35f;
     }
 
     public void SocketSelectEntered()
